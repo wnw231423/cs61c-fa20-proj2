98f0fee32a4ba44a4b7fd0de96326cd2e0f7fe49
wnw231423 <2684722187@qq.com>
2024-10-23 23:46:09 +0800

    finish read_matrix but more unittests need to be added.

--- proj2-notes.md
+++ proj2-notes.md
@@ -1,2 +1,7 @@
 Some side notes here:
-1. In unittest, use `t.execute(code=78)` to check if program exit with specific error. I learned it by reading fa24's unittest.py file.(https://github.com/61c-teach/fa24-proj2-starter/blob/main/unittests.py)
+1. In unittest, use `t.execute(code=78)` to check if program exit with specific error. I learned it by reading fa24's unittest.py file.(https://github.com/61c-teach/fa24-proj2-starter/blob/main/unittests.py).
+2. Bad experience when doing `write matrix`:
+    1. IDK the buffer can only be heap area so I directly do fread, passing the pointer of `rows` and `cols` to it, which cause errors.
+    2. IDK the output only contains elements of the matrix but not the "meta-data" like rows and cols so I return something like [2(rows), 2(cols), 1, 2, 3, 4], which cause errors.
+    3. I realizing that I was doing the wrong thing. Thus I became nervous and I forget "slli 2" when malloc, which makes me even sadder.
+   Hard as they were, tomorrow is another day :) 
--- src/read_matrix.s
+++ src/read_matrix.s
@@ -25,18 +25,94 @@
 #   this function terminates the program with error code 92.
 # ==============================================================================
 read_matrix:
-
     # Prologue
-	
+	addi sp, sp, -32
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
+    sw s4, 20(sp)
+    sw s5, 24(sp)
+    sw s6, 28(sp)
+    
+    # save parameters
+    mv s0, a0
+    mv s1, a1
+    mv s2, a2
+    
+    # do fopen
+    mv a1, s0
+    addi a2, x0, 0
+    jal ra, fopen
+    addi t0, x0, -1
+    beq a0, t0, error2
+    mv s5, a0  # s5 is the file descriptor
+    
+    # do fread to get the # of elements
+    addi a0, x0, 8
+    jal ra, malloc
+    beq a0, x0, error1
+    mv s6, a0  # s6 is the temp heap space to store rows and cols
+    
+    mv a1, s5
+    mv a2, s6
+    addi a3, x0, 8
+    jal ra, fread
+    addi t0, x0, 8
+    blt a0, t0, error3
+    
+    lw t0, 0(s6)
+    lw t1, 4(s6)
+    
+    sw t0, 0(s1)
+    sw t1, 0(s2)
 
-
-
-
-
-
-
-
+    mul s3, t0, t1  # the # of elements in matrix
+    
+    # call malloc for matrix
+    slli a0, s3, 2
+    jal ra, malloc
+    beq a0, x0, error1
+    mv s4, a0  # s4 is the pointer to the buffer
+    
+    # do fread and get exact matrix
+    mv a1, s5
+    mv a2, s4
+    slli a3, s3, 2
+    jal ra, fread
+    blt a0, s3, error3
+    
+    # do fclose
+    mv a1, s5
+    jal ra, fclose
+    bne a0, x0, error4
+    
+    # put return value
+    mv a0, s4
+    
     # Epilogue
-
-
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    lw s6, 28(sp)
+    addi sp, sp, 32
+    
     ret
+ 
+ error1:
+    addi a1, x0, 88
+    jal x0, exit2
+error2:
+    addi a1, x0, 90
+    jal x0, exit2
+error3:
+    addi a1, x0, 91
+    jal x0, exit2
+error4:
+    addi a1, x0, 92
+    jal x0, exit2
